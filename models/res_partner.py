7a0c96ab712226ca6f12ba9fe4385ca144cead82
Antonio Queb <antonioqueb@alphaqueb.online>
2025-01-31 02:34:52 -0600

    Up

--- models/res_partner.py
+++ models/res_partner.py
@@ -2,9 +2,10 @@ from odoo import models, api, exceptions, _
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-
-    @api.model
-    def create(self, vals):
-        if not self.env.user.has_group('contact_restriction.group_create_contact'):
-            raise exceptions.AccessError(_("⚠️ No tienes permiso para crear contactos. Contacta al administrador."))
-        return super().create(vals)
+    
+    # Se elimina la sobreescritura del método create
+    # @api.model
+    # def create(self, vals):
+    #     if not self.env.user.has_group('contact_restriction.group_create_contact'):
+    #         raise exceptions.AccessError(_("⚠️ No tienes permiso para crear contactos. Contacta al administrador."))
+    #     return super().create(vals)
